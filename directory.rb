74e31a01b51aeaeab54ddcb6c554900c095a4f8e
Rob Baile <robbaile12@outlook.com>
2019-10-01 21:08:58 +0100

    put code into methods

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_header
     puts "-------------"
   end
   
-  def input_students
+def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # create an empty array
@@ -15,34 +15,41 @@ def print_header
     cohort = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do  
-      # add the student hash to the array
-      students << {name: name, cohort: cohort}
-      puts "Now we have #{students.count} students"
-      # get another name from the user
-      puts "Please enter the names of the students"
-      name = gets.chomp
-      puts "Please enter which cohort you are in"
-      cohort = gets.chomp
+        # add the student hash to the array
+        students << {name: name, cohort: cohort}
+        puts "Now we have #{students.count} students"
+        # get another name from the user
+        puts "Please enter the names of the students"
+        name = gets.chomp
+        puts "Please enter which cohort you are in"
+        cohort = gets.chomp
     end
     # return the array of students
     students
-  end
+end
   
-  def print_footer(names)
+def print_footer(names)
     puts "Overall, we have #{names.count} great students"
-  end  
+end  
   
 print_header
 input_students
 print_footer(students)
 
 
-#   students.map.with_index(1) {|student, i| puts "#{i} - " + "#{student.values[0]}" if student.values[0][0] === "R" && student.values[0].length < 12 }
+# Get the students that start with R and thats names are shorter than 12 characters
+def print_with_index(students)
+    students.map.with_index(1) {|student, i| puts "#{i} - " + "#{student.values[0]}" if student.values[0][0] === "R" && student.values[0].length < 12 }
+end
 
-# counter = 0
-# while counter < students.size 
-#     keys = students.values_at(counter)
-#     puts "#{counter} - " + "#{keys[0].values[0]}" 
-#     counter +=1
-# end 
+def print_with_while(students)
+    counter = 0
+    while counter < students.size 
+        keys = students.values_at(counter)
+        puts "#{counter} - " + "#{keys[0].values[0]}" 
+        counter +=1
+    end 
+end
+
+
 

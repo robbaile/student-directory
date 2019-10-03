af47b401f5304759768eb840ae9cec8b140e42f6
Rob Baile <robbaile12@outlook.com>
2019-10-03 15:00:34 +0100

    added ability to load students from students.csv

--- typos.rb
+++ typos.rb
@@ -16,6 +16,8 @@ def process(selection)
       show_students()
     when "3"
       save_students()  
+    when "4"
+      load_students()  
     when "9"
       exit
     else
@@ -34,6 +36,15 @@ def input_students
     end
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
@@ -50,6 +61,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the students to students.csv"
+  puts "4. Load the students from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 

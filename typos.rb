b23197d9859e9c88a317e5620f78e2072451445b
Rob Baile <robbaile12@outlook.com>
2019-10-03 14:26:37 +0100

    added interactive menu

--- typos.rb
+++ typos.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -6,12 +31,12 @@ def input_students
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
-    while name.empty? do
+    while !name.empty? do
     # add the student hash to the array
-    students << {:name : name :cohort : :november}
-    puts "Now we have #{student.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # get another name from the user
-    name = get.chomp
+    name = gets.chomp
     end
     # return the array of students
     students
@@ -23,17 +48,17 @@ def print_header
 end
 
 def print(students)
-    student.each do |student|
-        puts "#{student{:name}} (#{student{:cohort}} cohort)"
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
 def print_footer names
-    print "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{names.count} great students"
 end
 
-
+interactive_menu()
 students = input_students
-print_header
+print_header()
 print(students)
 print_footer(students)
